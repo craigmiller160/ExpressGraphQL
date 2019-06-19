27d01973ef8fba703e97fa1201f59dea24890756
Craig Miller <craigmiller160@gmail.com>
2019-06-18 21:41:25 -0400

    More work on stuff

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,11 +1,16 @@
 import * as gulp from 'gulp';
 import * as typescript from 'gulp-typescript';
+import { Project } from 'gulp-typescript';
 
-const tsProject = typescript.createProject('tsconfig.json');
+const tsProject: Project = typescript.createProject('tsconfig.json');
 
 gulp.task('compile', () => {
     return gulp.src('src/**/*.ts')
         .pipe(tsProject())
         .js
         .pipe(gulp.dest('out'));
+});
+
+gulp.task('tslint', () => {
+    
 });
