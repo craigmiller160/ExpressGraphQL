43bb9af98834752f0b5d73be6103084b746a6d7f
Craig Miller <craigmiller160@gmail.com>
2019-06-22 17:03:37 -0400

    Added basic graphql support

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import graphqlHttp from 'express-graphql';
 
 const port: number = Number(process.env.PORT) || 3000;
 
@@ -7,8 +8,9 @@ const app: Express = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World');
-});
+app.use('/graphql', graphqlHttp({
+    schema: null,
+    rootValue: {}
+}));
 
 app.listen(port);
