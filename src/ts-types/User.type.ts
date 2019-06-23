ffaf24d65478a396e018531a9974a4911ceca8bc
Craig Miller <craigmiller160@gmail.com>
2019-06-23 14:27:19 -0400

    Fixed tslint issue

--- src/ts-types/User.type.ts
+++ src/ts-types/User.type.ts
@@ -3,5 +3,5 @@ import IBaseDb from './BaseDb.type';
 export default interface IUser extends IBaseDb {
     email: string;
     password?: string;
-    createdEvents: [any]
+    createdEvents: [any];
 }
