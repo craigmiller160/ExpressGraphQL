0bec4f1ec218bb16a24ed36c2b112cb2aa44ef7f
Craig Miller <craigmiller160@gmail.com>
2019-06-28 20:31:33 -0400

    Defined the booking TS type

--- src/ts-types/Booking.type.ts
+++ src/ts-types/Booking.type.ts
@@ -1,5 +1,8 @@
 import IBaseDb from './BaseDb.type';
+import IEvent from './Event.type';
+import IUser from './User.type';
 
 export default interface IBooking extends IBaseDb{
-
+    event: string | IEvent;
+    user: string | IUser;
 }
